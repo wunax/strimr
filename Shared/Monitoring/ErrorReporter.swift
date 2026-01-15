af0dabed53bb3b6191f0ec5292697d57a6dacf48
Wunax <wunax@protonmail.com>
2026-01-15 13:56:00 +0100

    style: format code

--- Shared/Monitoring/ErrorReporter.swift
+++ Shared/Monitoring/ErrorReporter.swift
@@ -3,20 +3,20 @@ import Foundation
 enum ErrorReporter {
     static func start() {
         #if canImport(Sentry)
-        SentrySDK.start { options in
-            options.dsn = Bundle.main.object(forInfoDictionaryKey: "SENTRY_DSN") as? String
-            options.enableLogs = true
-        }
+            SentrySDK.start { options in
+                options.dsn = Bundle.main.object(forInfoDictionaryKey: "SENTRY_DSN") as? String
+                options.enableLogs = true
+            }
         #endif
     }
 
     static func capture(_ error: Error) {
         #if canImport(Sentry)
-        SentrySDK.capture(error: error)
+            SentrySDK.capture(error: error)
         #endif
     }
 }
 
 #if canImport(Sentry)
-import Sentry
+    import Sentry
 #endif
